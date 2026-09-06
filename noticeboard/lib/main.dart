8b540ee6e71eb5405abb46de258db638697fa24b
Akshit517 <akshitmandial517@gmail.com>
2026-09-06 21:44:20 +0530

    fix: remove connectivity feature

--- noticeboard/lib/main.dart
+++ noticeboard/lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:noticeboard/bloc/connectivity_status_bloc.dart';
 import './routes/routing_constants.dart';
 import './routes/routing.dart';
 import 'global/global_constants.dart';
@@ -20,7 +19,6 @@ class _MyAppState extends State<MyApp> {
   // This widget is the root of your application.
   @override
   void initState() {
-    ConnectivityStatusBloc();
     super.initState();
   }
 
